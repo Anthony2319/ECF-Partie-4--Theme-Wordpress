c6819ed06d85a28cecb72f7969c3f8549004f2b2
AlexD <alexandredesoutter.pro@gmail.com>
2021-07-16 12:33:55 +0200

    first commit

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+get_header();
+
+?>
+<div class="container">
+    <article>
+        <div class="row">
+            <div class="col-sm">
+                <h1>404 Not found</h1>
+                <div>Désolé, la page est introuvable.</div>
+            </div>
+        </div>
+    </article>
+</div>
+<?php
+
+get_footer();
